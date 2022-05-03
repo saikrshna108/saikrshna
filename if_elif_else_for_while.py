18d588e51f69619b4bf8cf25b54a7d271337b7b2
saikrshna <saikrshna>
2022-05-03 10:03:14 +0530

    updated programs

---
+++ if_elif_else_for_while.py
@@ -0,0 +1,20 @@
+lst1 = [1,2]
+# for s in lst1:
+#     if s>3:
+#         print("grtr than 3")
+#     elif s==3:
+#         print("equals to 3")
+#     else:
+#         print("less than 3")
+# for s in lst1:
+#     if s%2 ==0:
+#         print("even number")  
+#     else:
+#         print("odd")
+
+
+''' temperature conversion'''
+# from celcius to forenheit
+c = [37,32,23,43,34]
+f = c*33.8
+print(f)
