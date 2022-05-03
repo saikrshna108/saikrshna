7d33e5c814ca11e5d9cde4f459aef5d52582ecad
saikrshna <saikrshna>
2022-05-03 10:05:09 +0530

    updated

---
+++ python/keywords.py
@@ -0,0 +1,10 @@
+# total number of keywords in python are 33.
+# they are...
+import keyword
+print(keyword.kwlist)
+
+'''['False', 'None', 'True', 'and', 'as', 'assert', 'async', 
+ 'await', 'break', 'class', 'continue', 'def', 
+ 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 
+'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 
+'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']'''
