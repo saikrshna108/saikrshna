452b30d2cbdfb30382404bfe29136a584fee28bb
saikrshna <saikrshna>
2022-05-03 10:56:38 +0530

    while loop

--- data_types.py
+++ data_types.py
@@ -1,10 +1,11 @@
-a = 50
-b = 45
-# print("A") if a>b else print("B")
-if a==b:
-    print("a=b")
-elif a>b:
-    print("a grtr than b")
-else:
-    print("a less than b")
+# a = 50
+# b = 45
+# # print("A") if a>b else print("B")
+# if a==b:
+#     print("a=b")
+# elif a>b:
+#     print("a grtr than b")
+# else:
+#     print("a less than b")
+
 
---
+++ python/while.py
@@ -0,0 +1,7 @@
+g=1
+while g<5:
+    print("go")
+    if g==3:
+        print("stop")
+    g+=1
+    break
